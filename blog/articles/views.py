2a2a2a5abbd4572edbc9461701f4aeebbb90460c
Flayzex <redkiyflayzex@gmail.com>
2023-08-15 11:52:00 +0500

    Connect templates to site

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,3 +1,13 @@
+from typing import Any, Dict
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
+
 # Create your views here.
+class ArticlesHomeView(TemplateView):
+    template_name = 'articles/articleshome.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        c_def = {'title': 'Главная страница'}
+        return context | c_def
