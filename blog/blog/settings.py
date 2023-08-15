7d6f261b676c44eae75a02245a8c4bca482ad8a6
Flayzex <redkiyflayzex@gmail.com>
2023-08-15 22:09:01 +0500

    Add django-debug-toolbar

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
+
     'articles.apps.ArticlesConfig'
 ]
 
@@ -39,6 +41,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
@@ -115,3 +119,7 @@ STATICFILES_DIRS = []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('articles.urls'))
 ]
+
+if DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
