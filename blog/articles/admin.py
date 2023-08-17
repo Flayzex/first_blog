a6a7b6c098045e05ac3df893f6248326fc64b08a
Flayzex <redkiyflayzex@gmail.com>
2023-08-17 22:49:42 +0500

    Update admin panel

--- blog/articles/admin.py
+++ blog/articles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'user', 'description', 'time_create')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
+    list_filter = ('time_create',)
+    prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'user', 'description', 'content', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update')
+
+
+admin.site.register(Articles, ArticlesAdmin)
+
+admin.site.site_title = 'Админ-панель статей'
